8ec18bac37a9e54687705221e3654706975ec1d9
BartramAlex <arb145@zips.uakron.edu>
2018-04-07 06:40:54 -0400

    Update code_analysis_t.cpp
    Option tag conditions work.

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -11,20 +11,21 @@
 
 int main() {
 
-    // all parts of the request are empty
+    // Objects with underscore 1 are the desired outcome of the tests
+	// This test is to assert that option cases take precedence in all analysis tests
     {
         analysis_request request;
-        request.given_filename  = "";
-        request.entry_filename  = "";
-        request.given_url       = "";
-        request.option_filename = "";
-        request.option_url      = "";
-        request.option_language = "";
+        request.given_filename  = "Filename_2";
+        request.entry_filename  = "Filename_3";
+        request.given_url       = "URL_2";
+        request.option_filename = "Filename_1";
+        request.option_url      = "URL_1";
+        request.option_language = "Language_1";
 
         auto filename = analysis_filename(request);
-        assert(filename == "");
-        assert(analysis_url(request) == "");
-        assert(analysis_language(request, filename) == "");
+        assert(filename == "Filename_1");
+        assert(analysis_url(request) == "URL_1");
+        assert(analysis_language(request, filename) == "Language_1");
         assert(code_analysis(request) == false);
     }
 
